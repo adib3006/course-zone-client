8bcdd21b725327d6843fd8790b7a738d74189a35
adib3006 <ma.adib3006@gmail.com>
2022-10-25 03:01:31 +0600

    logo and components and other folders created

---
+++ src/components/CourseCard.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const CourseCard = () => {
+    return (
+        <div>
+            <h4>Course</h4>
+        </div>
+    );
+};
+
+export default CourseCard;
---
+++ src/components/CoursesLeftSideBar.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const CoursesLeftSideBar = () => {
+    return (
+        <div>
+            <h3>Left side</h3>
+        </div>
+    );
+};
+
+export default CoursesLeftSideBar;
